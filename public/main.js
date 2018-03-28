530225e0ab1e368ef33da95e5ee97ff43d6d8416
Enoch <jenoch@proforma.com>
2018-03-27 23:17:06 -0400

    updated post

--- public/main.js
+++ public/main.js
@@ -1,5 +1,18 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
+const hackURL :metro-node-texting.herokuapp.com/send/message;
+var data = {
+    "number":"555-867-5309",
+    "message":"I need more cow bell!"
+    "name":"Chris Walken"
+};
 
-document.addEventListener('DOMContentLoaded', main)
+fetch(hackURL, {
+  method: 'POST', 
+  body: JSON.stringify(data), 
+  headers: new Headers({
+    'Content-Type': 'application/json'
+  })
+}).then(res => res.json())
+.catch(error => console.error('Error:', error))
+.then(response => console.log('Success:', response));
+
+// I don't know what the headers section is for? Is that pertinent/necessary?
