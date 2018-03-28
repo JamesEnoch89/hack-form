67a83c57d516244f7fc964dcfe17319234ef0a5c
Enoch <jenoch@proforma.com>
2018-03-28 15:41:42 -0400

    finished form

--- public/main.js
+++ public/main.js
@@ -1,18 +1,28 @@
-const hackURL :metro-node-texting.herokuapp.com/send/message;
-var data = {
-    "number":"555-867-5309",
-    "message":"I need more cow bell!"
-    "name":"Chris Walken"
-};
+const postForm = () =>{
 
-fetch(hackURL, {
-  method: 'POST', 
-  body: JSON.stringify(data), 
-  headers: new Headers({
-    'Content-Type': 'application/json'
-  })
-}).then(res => res.json())
-.catch(error => console.error('Error:', error))
-.then(response => console.log('Success:', response));
+  // const hackURL= "http://metro-node-texting.herokuapp.com/send/message";
 
-// I don't know what the headers section is for? Is that pertinent/necessary?
+  var data = {
+    "number": document.querySelector("#phone").value,
+    "name": document.querySelector("#name").value,
+    "message": document.querySelector("#request").value,
+  };
+  
+  // fetch(hackURL, {
+  //   method: 'POST',
+  //   body: JSON.stringify(data),
+  //   headers: new Headers({
+  //     'Content-Type': 'application/json'
+  //   })
+  // }).then(res => res.json())
+  // .catch(error => console.error('Error:', error))
+  // .then(response => console.log('Success:', response));
+
+  //document.querySelector("#thankyou").textContent = submitText;
+
+  document.querySelector("#form").classList.add("hideForm");
+  document.querySelector("#thankyou").classList.remove("hideForm");
+}
+
+
+
